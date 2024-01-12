bff870140ac4dc55d6c4e738c8b0ebd041999ef8
Orive2023 <orivesolutions8@gmail.com>
2024-01-12 18:16:17 +0530

    By Pritam

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,42 +1,35 @@
-import "./Navbar.css";
-import Logo from "../../assets/logo.png";
-import phone from "../../assets/phone.png";
-import { Link } from "react-router-dom";
-const Navbar = () => {
+import { useRef } from "react";
+import Logo from "../../assets/Logo.png"
+import phone from "../../assets/phone.png"
+import { FaBars } from "react-icons/fa";
+import "../Navbar/Navbar.css";
+
+function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
-    <div className="navbar">
-      <Link to="/">
-        <img src={Logo} alt="" className="logo" />
-      </Link>
-      <ul>
-        <Link to="/">
-          {" "}
-          <li>Adobe Leaf</li>
-        </Link>
-        <Link to="/who">
-          <li>Who We Are</li>
-        </Link>
-        <li className="dropdown">
-          Resources
-          <ul className="dropdown-content">
-            <li>Submenu Item 1</li>
-            <li>Submenu Item 2</li>
-          </ul>
-        </li>
-        <Link to="/journal">
-          <li className="dropdown">
-            Journals
-            <ul className="dropdown-content">
-            <Link to="/journalD"> <li>Journal Details</li> </Link>
-              <li>Submenu Item 2</li>
-            </ul>
-          </li>
-        </Link>
-        <Link to="/reachus">
-          <li>Reach Us</li>
-        </Link>
-      </ul>
-      <div className="contact-num">
+    <header>
+		<img src={Logo} alt="" className="logo"></img>
+      <nav ref={navRef}>
+        <a href="/">Adobe Leaf</a>
+        <a href="/who">Who we are</a>
+        <div className="dropdown">
+           <a> Resources
+          <div class="dropdown-content">
+            <a href="#">Services</a>
+            <a href="#">Products</a>
+          </div>
+		  </a>
+        </div>
+        <a href="/journal">Journal</a>
+        <a href="/reachus">Reach Us</a>
+      
+
+	  <div className="contact-num">
         <img src={phone} alt="" className="contact-img" />
         <div className="phoneNo">
           <p>+91 9777798142</p>
@@ -44,11 +37,16 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="button-connect" type="button">
-        <button>Let's Connect! </button>
+	  <div className="button-connect" type="button">
+        <button>Let's Connect!</button>
       </div>
-    </div>
+	</nav>
+
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
   );
-};
+}
 
 export default Navbar;
